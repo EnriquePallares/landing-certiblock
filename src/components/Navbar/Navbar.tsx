91a690d41db575fa5366add607465548555a2fa3
Santiago Martínez <xsantysxx@gmail.com>
2022-10-18 19:32:48 -0500

    Tailwind feature

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,8 +1,45 @@
-import React from 'react';
+import React from "react";
 export interface NavbarInterface {}
 
-const Navbar : React.FC<NavbarInterface> = () => {
-	return <div>Navbar</div>;
+const menuItems = [{}];
+
+const Navbar: React.FC<NavbarInterface> = () => {
+  return (
+    <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded">
+      <div className="container flex flex-wrap justify-between items-center mx-auto">
+        <a href="#" className="flex items-center">
+          <img
+            src="https://flowbite.com/docs/images/logo.svg"
+            className="mr-3 h-6 sm:h-9"
+            alt="Flowbite Logo"
+          />
+          <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+            Flowbite
+          </span>
+        </a>
+        <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+          <ul className="flex flex-col p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+            <li>
+              <a
+                href="#"
+                className="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
+                aria-current="page"
+              >
+                Home
+              </a>
+            </li>
+          </ul>
+        </div>
+        <button
+          type="button"
+          aria-controls="navbar-default"
+          aria-expanded="false"
+        >
+          <span className="sr-only">Open main menu</span>
+        </button>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
