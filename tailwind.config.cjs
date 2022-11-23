cff264ce79b0185fa0f631de97081add7d94cda4
EnriquePallares <eapallaress@gmail.com>
2022-11-23 12:47:34 -0500

    Hotfix

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    './index.html',
     './src/**/*.{js,jsx,ts,tsx}',
     'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
   ],
@@ -24,5 +25,9 @@ module.exports = {
       },
     },
   },
-  plugins: [require('flowbite/plugin'), require('@tailwindcss/line-clamp'), require('autoprefixer')],
+  plugins: [
+    require('flowbite/plugin'),
+    require('@tailwindcss/line-clamp'),
+    require('autoprefixer'),
+  ],
 };
