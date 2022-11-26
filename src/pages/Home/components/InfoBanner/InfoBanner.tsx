e0b2c831f89cd0918c5d6d10e96d12ee8a4c8c33
MiladyGomez <miladyg16@hotmailcom>
2022-11-26 11:06:12 -0500

    Components info Banner

--- src/pages/Home/components/InfoBanner/InfoBanner.tsx
+++ src/pages/Home/components/InfoBanner/InfoBanner.tsx
@@ -7,17 +7,17 @@ const InfoBanner: React.FC<InfoBannerInterface> = () => {
   return (
     <section
       id="info-banner"
-      className="extend-bg-color relative rounded-t-3xl bg-light-gray md:rounded-t-[4.5rem] md:bg-primary"
+      className="extend-bg-color relative bg-light-gray md:bg-primary"
     >
-      <div className="container relative mx-auto px-8 pb-10 md:px-14 md:py-20">
+      <div className="container relative mx-auto max-w-full"> 
         <img
           src={floatIcon}
           alt="Float icon"
-          className="absolute right-14 -top-14 hidden md:inline-block"
+          className="floatIcon absolute right-14 -top-14 hidden md:inline-block"
         />
 
         <div className="grid gap-10 font-medium md:grid-cols-2">
-          <div className="text-section rounded-3xl pt-12 text-white md:bg-white md:py-12 md:px-6 md:text-dark-blue-4">
+          <div className="text-section text-white md:bg-white md:text-dark-blue-4">
             <h4 className="mb-8 text-4xl">
               Certificamos <br /> desde el origen
             </h4>
